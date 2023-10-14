3edb73e10e40565d1d745020c61de83908297aed
Gabriel Padilha Santos <gabrielpadilhasantos@gmail.com>
2023-10-14 20:36:27 -0300

    Delete maven-empty-project.sh

--- maven-empty-project.sh
+++
@@ -1,5 +0,0 @@
-  1 #!/bin/bash
-  2 #
-  3 mvn archetype:generate -DgroupId=io.github.gabrielpadilh4 -DartifactId=wildfly-elytron-credential-store-example -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false                           
-~                                                                                                                                                                                                                                             
-~                    
